6baa61f6367f6fe39b165404a6db3758f3e98e1e
Federico Ramallo <framallo@gmail.com>
2015-08-23 13:52:51 -0500

    test rake db:seed task

--- lib/tasks/db.rake
+++ lib/tasks/db.rake
@@ -1,8 +1,8 @@
 desc 'seed database'
 namespace :db do
-  task :seed do
+  task :seed => :environment do
     seed = File.read File.join(Rails.root, 'db','seed.graph')
     Neo4j::Session.query seed
-    Rake::Task['neo4j:migrate[add_id_property]'].invoke
+    Rake::Task['neo4j:migrate'].invoke 'add_id_property'
   end
 end
--- spec/lib/tasks/db_spec.rb
+++ spec/lib/tasks/db_spec.rb
@@ -4,10 +4,15 @@ describe 'db:seed', type: :rake do
   it { is_expected.to depend_on(:environment) }
 
   it 'populates the database' do
+    person_count = Person.count
+    movie_count  = Movie.count
+
     subject.execute
 
-    expect(Person.count).to eq(131)
-    expect(Movie.count).to eq(38)
+    expect(Person.count - person_count).to eq(131)
+    expect(Movie.count  - movie_count ).to eq(38)
+
+
   end
 
 end
