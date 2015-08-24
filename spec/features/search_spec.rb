f5d35660e795685142b927be2410a9a8c90c257d
Federico Ramallo <framallo@gmail.com>
2015-08-24 14:53:35 -0500

    fixes search spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -6,12 +6,6 @@ feature 'Movies' do
     fill_in 'Search', with: 'Top'
     click_button 'Submit'
 
-    #the_matrix = find(:xpath, "//div[@class='list-group']/a[@href='/movies/The%20Matrix']")
-    #expect(the_matrix.text).to eq "The Matrix 5"
-
-    ##binding.pry
-
-    #click_link "The Matrix 5"
     expect(page).to have_css('.list-group-item', count: 1)
   end
 end
