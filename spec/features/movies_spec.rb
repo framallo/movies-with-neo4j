78b14befbf97ad57c51cfd93b7ec14080b7694f0
Federico Ramallo <framallo@gmail.com>
2015-08-24 12:31:51 -0500

    add movie feature spec

---
+++ spec/features/movies_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'Movies' do
+  scenario "Browse movies" do
+    visit '/movies'
+
+    the_matrix = find(:xpath, "//div[@class='list-group']/a[@href='/movies/The%20Matrix']")
+    expect(the_matrix.text).to eq "The Matrix 5"
+
+    #binding.pry
+
+    click_link "The Matrix 5"
+    expect(page).to have_content('Welcome to the Real World')
+  end
+end
