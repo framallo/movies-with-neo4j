be8cd41bc6a7ad4020009e218ece5415f00a6616
Federico Ramallo <framallo@gmail.com>
2015-08-25 17:48:25 -0500

    fixes movies integration test

--- spec/features/movies_spec.rb
+++ spec/features/movies_spec.rb
@@ -5,9 +5,9 @@ feature 'Movies' do
     visit '/movies'
 
     the_matrix = find(:xpath, "//div[@class='list-group']/a[@href='/movies/The%20Matrix']")
-    expect(the_matrix.text).to eq "The Matrix 5"
+    expect(the_matrix.text).to eq "The Matrix 8"
 
-    click_link "The Matrix 5"
+    click_link "The Matrix 8"
     expect(page).to have_content('Welcome to the Real World')
   end
 end
